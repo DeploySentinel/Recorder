2c1cd9a35e8ad3f5f6974c1609c660fcc7df28fc
warren <warren@pop-os.localdomain>
2022-03-16 21:37:22 -0700

    test: CypressScriptBuilder

--- tests/builders.test.ts
+++ tests/builders.test.ts
@@ -1,6 +1,83 @@
-import { PlaywrightScriptBuilder, PuppeteerScriptBuilder } from '../src/pages/builders';
+import { CypressScriptBuilder, PlaywrightScriptBuilder, PuppeteerScriptBuilder } from '../src/pages/builders';
 
 describe('Test builders', () => {
+  describe('CypressScriptBuilder', () => {
+    let builder: any;
+    let mockPushCodes: any;
+
+    beforeEach(() => {
+      builder = new CypressScriptBuilder();
+      mockPushCodes = jest.spyOn(builder, 'pushCodes').mockReturnValue(builder);
+    });
+
+    test('pushComments, pushCodes and build', () => {
+      mockPushCodes.mockRestore();
+
+      const output = builder
+        .pushComments('// hello-world')
+        .pushCodes('cy.visit()')
+        .build()
+      expect(output).toBe(
+        `it('Test with DeploySentinel', () => {\n  // hello-world\n  cy.visit()\n})`
+      );
+    });
+
+    test('click', () => {
+      builder.click('selector', true);
+      expect(mockPushCodes).toHaveBeenNthCalledWith(1, "cy.get('selector').click()");
+    });
+
+    test('hover', () => {
+      builder.hover('selector', true);
+      expect(mockPushCodes).toHaveBeenNthCalledWith(1, "cy.get('selector').trigger('mouseover')");
+    });
+
+    test('load', () => {
+      builder.load('url');
+      expect(mockPushCodes).toHaveBeenNthCalledWith(1, "cy.visit('url')");
+    });
+
+    test('resize', () => {
+      builder.resize(1, 2);
+      expect(mockPushCodes).toHaveBeenNthCalledWith(1, "cy.viewport(1, 2)");
+    });
+
+    test('fill', () => {
+      builder.fill('selector', 'value', true);
+      expect(mockPushCodes).toHaveBeenNthCalledWith(1, "cy.get('selector').type('value')");
+    });
+
+    test('type', () => {
+      builder.type('selector', 'value', true);
+      expect(mockPushCodes).toHaveBeenNthCalledWith(1, "cy.get('selector').type('value')");
+    });
+
+    test('select', () => {
+      builder.select('selector', 'option', true);
+      expect(mockPushCodes).toHaveBeenNthCalledWith(1, "cy.get('selector').select('option')");
+    });
+
+    test('keydown', () => {
+      builder.keydown('selector', 'Enter', true);
+      expect(mockPushCodes).toHaveBeenNthCalledWith(1, "cy.get('selector').type('{Enter}')");
+    });
+
+    test('wheel', () => {
+      builder.wheel(1.2, 1.6);
+      expect(mockPushCodes).toHaveBeenNthCalledWith(1, "cy.scrollTo(1, 1)");
+    });
+
+    test('fullScreenshot', () => {
+      builder.fullScreenshot();
+      expect(mockPushCodes).toHaveBeenNthCalledWith(1, "cy.screenshot('screenshot.png')");
+    });
+
+    test('awaitText', () => {
+      builder.awaitText('text');
+      expect(mockPushCodes).toHaveBeenNthCalledWith(1, "cy.contains('text')");
+    });
+  });
+
   describe('PlaywrightScriptBuilder', () => {
     let builder: any;
     let mockWaitForActionAndNavigation: any;
